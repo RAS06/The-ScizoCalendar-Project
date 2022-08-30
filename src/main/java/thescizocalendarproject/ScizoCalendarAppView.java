c5211f3c685dc25c565791878cc1fcebe591da4a
Rizwan Syed <75791780+RAS06@users.noreply.github.com>
2022-08-30 13:26:01 -0400

    More config pushes

--- src/main/java/thescizocalendarproject/ScizoCalendarAppView.java
+++ src/main/java/thescizocalendarproject/ScizoCalendarAppView.java
@@ -1,4 +1,4 @@
-package com.example.thescizocalendarproject;
+package thescizocalendarproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- src/main/java/thescizocalendarproject/ScizoCalendarController.java
+++ src/main/java/thescizocalendarproject/ScizoCalendarController.java
@@ -1,4 +1,4 @@
-package com.example.thescizocalendarproject;
+package thescizocalendarproject;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
